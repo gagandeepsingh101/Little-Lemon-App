88c3f0372cd36c77b067bdadb5cc14f5495b5ea3
Gagandeep Singh <gagandeepsingh128548@gmail.com>
2024-10-04 14:52:03 +0530

    using Pressable  button

--- components/LittleLemonLoginPage.tsx
+++ components/LittleLemonLoginPage.tsx
@@ -1,17 +1,28 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, TextInput, View } from 'react-native'
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
 
 type Props = {}
 
 const LittleLemonLoginPage = (props: Props) => {
     const [email, setEmail] = useState<string>();
     const [password, setPassword] = useState<string>();
+    const [isLogin, setIsLogin] = useState<boolean>(false);
     return (
         <View style={loginPageStyle.container}>
             <Text style={loginPageStyle.headingStyle}>Welcome to Little Lemon</Text>
-            <Text style={loginPageStyle.titleStyle}>Login to Contiue</Text>
-            <TextInput value={email} onChangeText={setEmail} placeholder='Email' style={loginPageStyle.textInput} />
-            <TextInput secureTextEntry maxLength={8} value={password} onChangeText={setPassword} placeholder='Password' style={loginPageStyle.textInput} />
+            <Text style={loginPageStyle.titleStyle}>{isLogin ? "Welcome to Login Page" : "Login to Contiue"}</Text>
+            {!isLogin && <>
+                <TextInput value={email} onChangeText={setEmail} placeholder='Email' style={loginPageStyle.textInput} />
+                <TextInput secureTextEntry maxLength={8} value={password} onChangeText={setPassword} placeholder='Password' style={loginPageStyle.textInput} />
+                <Pressable style={loginPageStyle.loginButton} onPress={() => {
+                    if (email !== "" && password !== "") {
+                        setIsLogin(!isLogin)
+                    }
+                }} >
+                    <Text style={loginPageStyle.loginButtonText}>Login</Text>
+                </Pressable >
+            </>
+            }
         </View>
     )
 }
@@ -23,7 +34,7 @@ const loginPageStyle = StyleSheet.create({
         alignItems: "center",
     },
     textInput: {
-        marginVertical:10,
+        marginVertical: 10,
         backgroundColor: "#ededed",
         width: "80%",
         height: 40,
@@ -43,6 +54,17 @@ const loginPageStyle = StyleSheet.create({
         marginVertical: 8,
         color: '#EDEFEE',
         textAlign: 'center',
+    },
+    loginButton: {
+        backgroundColor: "#EE9972",
+        paddingVertical: 5,
+        paddingHorizontal: 30,
+        borderRadius: 50,
+        marginVertical:20
+    },
+    loginButtonText: {
+        fontSize: 28,
+        color: '#EDEFEE',
     }
 })
 export default LittleLemonLoginPage
